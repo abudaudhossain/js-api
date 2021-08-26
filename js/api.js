2ada8ea1d54509452b051909ce5055c89c8e57e9
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-26 16:49:42 +0600

    display data

--- js/api.js
+++ js/api.js
@@ -1,5 +1,24 @@
-function loadData(){
+function loadData() {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json))
+        .then(response => response.json())
+        .then(json => console.log(json))
 }
+
+function loadUser() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(json => displayUser(json));
+}
+
+
+
+function displayUser(data) {
+    const ul = document.getElementById('users');
+
+    for (const user of data) {
+        const li = document.createElement('li');
+        li.innerText = `Name: ${user.name}  email: ${user.email}`;
+        ul.appendChild(li);
+    }
+}
+
